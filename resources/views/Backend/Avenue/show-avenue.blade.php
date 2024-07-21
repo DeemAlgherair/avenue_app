0c82ee03f3da96992c28ad68c40222aca7d5903e
411202095@qu.edu.sa <411202095@qu.edu.sa>
2024-07-21 22:11:47 +0300

    3 commit

--- resources/views/Backend/Avenue/show-avenue.blade.php
+++ resources/views/Backend/Avenue/show-avenue.blade.php
@@ -2,7 +2,6 @@
 @section('content')
 <div class="container-fluid">
 
-    <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Avenues</h1>
         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
